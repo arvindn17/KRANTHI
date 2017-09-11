8a8e4053e682a3bc2fcfd5e3f0b3d9140ef5dfd9
arvindnegi17 <arvind.negi@tatrasdata.com>
2017-09-11 23:20:18 +0530

    route add

--- config/routes.php
+++ config/routes.php
@@ -75,6 +75,23 @@ Router::scope('/', function (RouteBuilder $routes) {
     $routes->fallbacks(DashedRoute::class);
 });
 
+Router::prefix('admin', function($routes) {
+    $routes->connect(
+        '/',
+        ['controller' => 'AdminUsers', 'action' => 'login']
+    );
+    $routes->connect(
+        '/:controller',
+        ['action' => 'index'],
+        ['routeClass' => DashedRoute::class]
+    );
+    $routes->connect(
+        '/:controller/:action/*',
+        [],
+        ['routeClass' => DashedRoute::class]
+    );
+ });
+
 /**
  * Load all plugin routes. See the Plugin documentation on
  * how to customize the loading of plugin routes.
